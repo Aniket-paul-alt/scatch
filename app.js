b58e86877734a9396515d69fe0c2f2ad0639e5d0
Aniket-paul-alt <113275960+Aniket-paul-alt@users.noreply.github.com>
2024-06-28 21:49:32 +0530

    separation & concerns

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express();
-const port = 3000
+const port = 3000;
+
+const db = require("./config/mongoose.connection.js")
 
 const cookieParser = require('cookie-parser')
 const path = require('path');
+
+const ownersRouter = require('./routes/ownersRouter.js')
+const productsRouter = require('./routes/productsRouter.js')
+const usersRouter = require('./routes/usersRouter.js')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -11,9 +17,14 @@ app.use(cookieParser)
 app.use(express.static(path.join(__dirname,"public")))
 app.set("view engine", "ejs")
 
-app.get('/',(req, res)=>{
+app.use("/owners", ownersRouter)
+app.use("/users", usersRouter)
+app.use("/products", productsRouter)
+
+app.get('/', (req,res)=>{
     res.send("hey")
 })
+
 
 app.listen(port,()=>{
     console.log("app is listening port : "+port);
